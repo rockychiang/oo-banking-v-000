8922a663cd0ed865c079ce62fa92502fa1014593
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-14 23:25:48 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
       if self.valid? && @sender.balance > @amount
         @sender.balance -= amount
         @receiver.balance += amount
-        self.status = ""
+        self.status = "complete"
       else
         "Transaction rejected. Please check your account balance."
         self.status = "rejected"
