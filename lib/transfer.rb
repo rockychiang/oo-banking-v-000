6d827a30a1852a5e52db655edd4a8874bc49efb0
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-14 23:24:39 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @status = "pending" 
+    if @status == "pending" 
       if self.valid? && @sender.balance > @amount
         @sender.balance -= amount
         @receiver.balance += amount
