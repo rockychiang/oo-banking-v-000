fb313eb68c150eb5eeecab0bac1f87ffd21142e6
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-14 23:20:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
       @sender.balance -= amount
       @receiver.balance += amount
     else
-      
+      "Transaction rejected. Please check your account balance."
     end
   end
   
